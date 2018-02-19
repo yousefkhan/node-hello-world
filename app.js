c4d775ffda3273e1c720ba6479456ff23e6313a4
GLOBALRESCUE\ykhan <>
2018-02-19 11:09:43 +0000

    port changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express()
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
-//Launch listening server on port 8080
-app.listen(8080, function () {
-    console.log('App listening on port 8080!')
+//Launch listening server on port 3000
+app.listen(3000, function () {
+    console.log('App listening on port 3000!')
 })
--- test/test-pages.js
+++ test/test-pages.js
@@ -2,21 +2,21 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
+    request('http://localhost:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
     });
 });
 
 it('Main page status', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
+    request('http://localhost:3000' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('About page content', function(done) {
-    request('http://localhost:8080/about' , function(error, response, body) {
+    request('http://localhost:3000/about' , function(error, response, body) {
         expect(response.statusCode).to.equal(404);
         done();
     });
