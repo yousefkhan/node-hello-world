6ad4839768b90ca12e27a88ef9cb6c14c4db7ef9
GLOBALRESCUE\ykhan <>
2018-02-19 12:03:02 +0000

    added test

--- test/test-pages.js
+++ test/test-pages.js
@@ -9,3 +9,12 @@ it('Should be string', function(done) {
         done();
     });*/
 });
+
+it('Should be string 2', function(done) {
+    expect("asdfj").to.be.a('string');
+    done();
+    /*request('http://localhost:3000' , function(error, response, body) {
+        expect(body).to.equal('Hello World');
+        done();
+    });*/
+});
