56c08c2380ea33477144c6cb3d87c8c5a0392f0a
GLOBALRESCUE\ykhan <>
2018-02-19 12:25:40 +0000

    added test

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Should be string', function(done) {
-    expect("alsdjf").to.be.a('string');
+    expect("alsdf").to.be.a('string');
     done();
     /*request('http://localhost:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World');
