7bfbd874c07ba946e2c120a9b83f3d3b5c1f5b7f
GLOBALRESCUE\ykhan <>
2018-02-19 12:22:08 +0000

    added test

--- test/test-pages.js
+++ test/test-pages.js
@@ -11,7 +11,7 @@ it('Should be string', function(done) {
 });
 
 it('Should be string 2', function(done) {
-    expect("asdfj").to.be.a('string');
+    expect("adfj").to.be.a('string');
     done();
     /*request('http://localhost:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World');
