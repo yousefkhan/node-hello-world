247e61c85a500694496f86945fc24654054ddd03
GLOBALRESCUE\ykhan <>
2018-02-20 04:40:38 +0000

    small

--- test/test-pages.js
+++ test/test-pages.js
@@ -11,7 +11,7 @@ it('Should be string', function(done) {
 });
 
 it('Should be string 2', function(done) {
-    expect("adfj").to.be.a('string');
+    expect("afj").to.be.a('string');
     done();
     /*request('http://localhost:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World');
